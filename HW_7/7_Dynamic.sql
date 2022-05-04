15c874a0f8cbd0afc8f42ccc0756ee85f965bed3
Eklerka466 <75840453+Eklerka466@users.noreply.github.com>
2022-05-05 00:23:12 +0300

    Create 7_Dynamic.sql

---
+++ HW_7/7_Dynamic.sql
@@ -0,0 +1,70 @@
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+
+Занятие "07 - Динамический SQL".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+
+Это задание из занятия "Операторы CROSS APPLY, PIVOT, UNPIVOT."
+Нужно для него написать динамический PIVOT, отображающий результаты по всем клиентам.
+Имя клиента указывать полностью из поля CustomerName.
+
+Требуется написать запрос, который в результате своего выполнения 
+формирует сводку по количеству покупок в разрезе клиентов и месяцев.
+В строках должны быть месяцы (дата начала месяца), в столбцах - клиенты.
+
+Дата должна иметь формат dd.mm.yyyy, например, 25.12.2019.
+
+Пример, как должны выглядеть результаты:
+-------------+--------------------+--------------------+----------------+----------------------
+InvoiceMonth | Aakriti Byrraju    | Abel Spirlea       | Abel Tatarescu | ... (другие клиенты)
+-------------+--------------------+--------------------+----------------+----------------------
+01.01.2013   |      3             |        1           |      4         | ...
+01.02.2013   |      7             |        3           |      4         | ...
+-------------+--------------------+--------------------+----------------+----------------------
+*/
+
+
+Declare @allCustomers nvarchar(max) = '[' +
+(
+	Select String_Agg(Cast(customer.CustomerName as nvarchar(max)), '], [')
+	From Sales.Customers customer
+) + ']'
+
+
+Declare @sql nvarchar(max) =
+';With pivotData as
+(
+	Select customer.CustomerID, Format(invoce.InvoiceDate, ''01.MM.yy'') [Month], customer.CustomerName
+	From Sales.Customers customer
+	Inner join Sales.Invoices invoce on customer.CustomerID = invoce.CustomerID
+)
+Select * 
+From pivotData
+Pivot 
+(
+	Count(pivotData.CustomerID) 
+	For pivotData.CustomerName 
+	in (' + @allCustomers + ')
+) pvt
+Order by [Month]'
+
+
+EXEC sp_executesql @sql
